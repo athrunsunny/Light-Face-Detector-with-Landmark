c630a6fa5cea4771c97eb566bfb5eb4f2df2455e
athrunsunny <43680123+athrunsunny@users.noreply.github.com>
2022-11-25 16:37:07 +0800

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,6 @@
+import platform
 
+
+def emojis(str=''):
+    # Return platform-dependent emoji-safe version of string
+    return str.encode().decode('ascii', 'ignore') if platform.system() == 'Windows' else str
