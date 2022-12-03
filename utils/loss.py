6a5bdf34a7022c74c5e3fd4178a2d34a652cc5ba
athrunsunny <43680123+athrunsunny@users.noreply.github.com>
2022-12-03 11:59:36 +0800

    Update loss.py

--- utils/loss.py
+++ utils/loss.py
@@ -6,17 +6,14 @@ from utils import box_utils
 
 
 class MultiboxLoss(nn.Module):
-    def __init__(self, priors, neg_pos_ratio,
-                 center_variance, size_variance, device):
+    def __init__(self, priors, hyp, device):
         """Implement SSD Multibox Loss.
 
         Basically, Multibox loss combines classification loss
          and Smooth L1 regression loss.
         """
         super(MultiboxLoss, self).__init__()
-        self.neg_pos_ratio = neg_pos_ratio
-        self.center_variance = center_variance
-        self.size_variance = size_variance
+        self.hyp = hyp
         self.priors = priors
         self.priors.to(device)
 
@@ -31,8 +28,9 @@ class MultiboxLoss(nn.Module):
         """
         num_classes = confidence.size(2)
         with torch.no_grad():
+            # derived from cross_entropy=sum(log(p))
             loss = -F.log_softmax(confidence, dim=2)[:, :, 0]
-            mask = box_utils.hard_negative_mining(loss, labels, self.neg_pos_ratio)
+            mask = box_utils.hard_negative_mining(loss, labels, self.hyp['neg_pos_ratio'])
 
         confidence = confidence[mask, :]
         classification_loss = F.cross_entropy(confidence.reshape(-1, num_classes), labels[mask].to(torch.long), reduction='sum')
@@ -41,4 +39,5 @@ class MultiboxLoss(nn.Module):
         gt_locations = gt_locations[pos_mask, :].reshape(-1, 4)
         smooth_l1_loss = F.smooth_l1_loss(predicted_locations, gt_locations, reduction='sum')  # smooth_l1_loss
         num_pos = gt_locations.size(0)
-        return smooth_l1_loss / num_pos, classification_loss / num_pos
+
+        return (smooth_l1_loss / num_pos) * self.hyp['box'], (classification_loss / num_pos) * self.hyp['cls']
